dd5a46c3c256566487f047fede8f71010682edea
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-07 16:48:29 +0200

    [FIXED] exercise fixed

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //stampi i numeri da 1 a 100,
 
 for(let i = 1; i<=100; i++){
-    
-    if ((i % 3) ===0){
+
+    if ((i % 3) ===0 && (i % 5)===0){
+        console.log("FrizzBuzz");
+        //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    }else if((i%3)===0){
         console.log("Frizz");
         //ma per i multipli di 3 stampi “Fizz” al posto del numero 
     }else if((i % 5) ===0){
         console.log("Buzz");
         //e per i multipli di 5 stampi Buzz.
-    }else if((i % 3) ===0 && (i % 5)){
-        console.log("FrizzBuzz");
-        //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
     }else{
         console.log(i);
-        
+
     }
 }
 
